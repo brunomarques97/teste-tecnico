604a9bef5cccf7a3b7e92e6e02cb557e4a62e3e8
brunomarques97 <brunomarques266@gmail.com>
2024-09-10 11:27:19 -0300

    Exercicio 2

--- index.js
+++ index.js
@@ -6,4 +6,34 @@ let k = 0;
 while ( k < indice) {
     k = k + 1; soma = soma + k; 
 }
-console.log(soma)
+document.getElementById('exercicio1').textContent = soma;
+
+
+//exercicio 2
+function verificarFibonacci() {
+    const numeroInformado =  parseInt(document.getElementById('numero').value);
+    
+    if (pertenceFibonacci(numeroInformado)) {
+        document.getElementById('exercicio2').textContent = numeroInformado + " pertence à sequência de Fibonacci.";
+    } else {
+        document.getElementById('exercicio2').textContent = numeroInformado + " não pertence à sequência de Fibonacci.";
+    }
+}
+
+function pertenceFibonacci(numero) {
+    let a = 0;
+    let b = 1;
+  
+    while (b <= numero) {
+      if (b === numero) {
+        return true;
+      }
+      let temp = b;
+      b = a + b;
+      a = temp;
+    }
+  
+    return false; 
+  }
+  
+
